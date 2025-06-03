c11295ff662455f6e12af417066009b75b1ef467
ParoVchiK <asd1234esds@gmail.com>
2025-06-04 01:39:14 +0300

    test_push

---
+++ api/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from main import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        tested_app.config['TESTING'] = True
+        self.app = tested_app.test_client()
+
+    def test_get_hello_endpoint(self):
+        r = self.app.get('/')
+        self.assertEqual(r.data, b'Hello World!')
+
+if __name__ == '__main__':
+    unittest.main()
