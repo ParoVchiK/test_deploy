c74e762512706660fbe0bacde945d6caecb32043
ParoVchiK <asd1234esds@gmail.com>
2025-06-03 13:11:55 +0300

    deploy test

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import unittest
-import app as tested_app
+from main import app as tested_app
 import json
 
 class FlaskAppTests(unittest.TestCase):
 
     def setUp(self):
-        tested_app.app.config['TESTING'] = True
-        self.app = tested_app.app.test_client()
+        tested_app.config['TESTING'] = True
+        self.app = tested_app.test_client()
 
     def test_get_hello_endpoint(self):
         r = self.app.get('/')
